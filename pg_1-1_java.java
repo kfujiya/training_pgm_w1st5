5443f0bd6b8ded58072f39fcaea386af5fa53c11
Alpierre Alvarez <a-aguilar@wiss1.co.jp>
2023-04-08 12:52:24 +0900

    本日の研修の作成したファイル

---
+++ pg_1-1_java.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Your code here!
+        
+        int i;
+        
+        for ( i = 1; i <= 9; i++){
+        
+        System.out.println(i);
+        
+            
+        }
+    }
+}
---
+++ pg_1-3_java.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Your code here!
+        
+        int i;
+        
+        for (i = 1; i <= 100; i++){
+            
+            String word = Integer.toString(i);
+            
+            if ( i % 3 == 0){
+            
+                System.out.println("hoge");
+                
+                continue;
+            }
+            
+            if ( word.contains("3")){
+             
+                System.out.println("hoge");
+                
+                continue;
+            }
+            
+            System.out.println(word);
+            
+        }
+    }
+}
