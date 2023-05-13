c974ec063f2112cf05be7717accd51929a42b8f0
Miho Sakuma <m-sakuma@wiss1.co.jp>
2023-05-13 15:15:37 +0900

    4月分課題提出

---
+++ pg1-1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Your code here!
+        int i ;
+        for (i=1; i<=10; i++) {
+            System.out.println( i );
+        }
+        
+    }
+}
---
+++ pg1-2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Your code here!
+            for (int i=1; i<=9; i++) {
+                for (int n=1; n<=9; n++) {
+                    System.out.printf(" %3d", (i * n));
+                }
+            System.out.println();
+        }
+    }
+}
